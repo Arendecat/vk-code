756c4b4a2aa797ad1bd4bc054b42744b28e3a482
Arendecat <lomanov27@gmail.com>
2021-11-05 12:33:19 +0300

    Initial Commit

---
+++ vk-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-code
+//
+//  Created by Danislav on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
