37d53c78250b02858bb3bc9520d8ad3ba830cfd2
Arendecat <lomanov27@gmail.com>
2022-02-09 16:03:33 +0300

    Сетевой сервис вынесен в отдельный класс, обновление данных в UI по запросу.

--- vk-code/Info/InfoViewController.swift
+++ vk-code/Info/InfoViewController.swift
@@ -2,14 +2,11 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    let url1 = URL(string: "https://jsonplaceholder.typicode.com/todos/")!
-    let url2 = URL(string: "https://swapi.dev/api/planets/1")!
-    
     private lazy var downloadButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Download data", for: .normal)
+        button.setTitle("Refresh data", for: .normal)
         button.backgroundColor = .systemBlue
-        button.addTarget(self, action: #selector(download), for: .touchUpInside)
+        button.addTarget(self, action: #selector(refresh), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -29,84 +26,15 @@ class InfoViewController: UIViewController {
         return label
     }()
     
-    struct ToDoModel: Decodable {
-        let userId: Int
-        let id: Int
-        let title: String
-        let completed: Bool
-    }
+    let networkService = InfoNetworkService()
     
-    struct PlanetModel: Decodable {
-        let planet: String
-        let orbitalPeriod: String
-        
-        enum CodingKeys: String, CodingKey{
-            case planet = "name"
-            case orbitalPeriod = "orbital_period"
-        }
-    }
+
     
-    
-    func call1() {
-        let group1 = DispatchGroup()
-        let queue1 = DispatchQueue(label: "call1", qos: .utility)
-        let decoder = JSONDecoder()
-        let config = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = 10
-        var request = URLRequest(url: url1)
-        request.httpMethod = "GET"
-        let session = URLSession(configuration: config)
-        var fetchedModels: [ToDoModel]? = nil
-        let task = session.dataTask(with: request) {data, response, error in
-            do {
-                try fetchedModels = decoder.decode([ToDoModel].self, from: data!)
-            } catch {print(error)}
-            group1.leave()
-        }
-        group1.enter()
-        queue1.async {
-            task.resume()
-        }
-        task.resume()
-        group1.notify(queue: .main) {
-            if (fetchedModels != nil) {
-                self.taskTitle.text = fetchedModels![0].title
-            } else { print("unable to download data") }
-        }
-        return
-    }
-    
-    func call2() {
-        let group2 = DispatchGroup()
-        let queue2 = DispatchQueue(label: "call2", qos: .utility)
-        let decoder = JSONDecoder()
-        let config = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = 10
-        var request = URLRequest(url: url2)
-        request.httpMethod = "GET"
-        let session = URLSession(configuration: config)
-        var fetchedModels: PlanetModel? = nil
-        let task = session.dataTask(with: request) {data, response, error in
-            do {
-                try fetchedModels = decoder.decode(PlanetModel.self, from: data!)
-            } catch {print(error)}
-            group2.leave()
-        }
-        group2.enter()
-        queue2.async {
-            task.resume()
-        }
-        group2.notify(queue: .main) {
-            if (fetchedModels != nil) {
-                self.planetPeriodLabel.text = "Планета " + fetchedModels!.planet + " имеет период обращения " + fetchedModels!.orbitalPeriod
-            } else { print("unable to download data") }
-        }
-        return
-    }
-    
-    @objc func download(){
-        call1()
-        call2()
+    @objc func refresh(){
+        networkService.call1()
+        networkService.call2()
+        self.taskTitle.text = networkService.call1Value
+        self.planetPeriodLabel.text = networkService.call2Value
     }
     
     override func viewDidLoad() {
@@ -127,5 +55,103 @@ class InfoViewController: UIViewController {
             planetPeriodLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
             planetPeriodLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
         ])
+    }
+}
+
+
+class InfoNetworkService {
+    
+    init() {
+        call1()
+        call2()
+    }
+    
+    let url1 = URL(string: "https://jsonplaceholder.typicode.com/todos/")!
+    let url2 = URL(string: "https://swapi.dev/api/planets/1")!
+    
+    struct ToDoModel: Decodable {
+        let userId: Int
+        let id: Int
+        let title: String
+        let completed: Bool
+    }
+    
+    struct PlanetModel: Decodable {
+        let planet: String
+        let orbitalPeriod: String
+        
+        enum CodingKeys: String, CodingKey{
+            case planet = "name"
+            case orbitalPeriod = "orbital_period"
+        }
+    }
+    
+    var call1Value: String = "Не удалось загрузить данные"
+    var call2Value: String = "Не удалось загрузить данные"
+    
+    
+    func call1() {
+        let group1 = DispatchGroup()
+        let queue1 = DispatchQueue(label: "call1", qos: .utility)
+        let decoder = JSONDecoder()
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 10
+        var request = URLRequest(url: url1)
+        request.httpMethod = "GET"
+        let session = URLSession(configuration: config)
+        var fetchedModels: [ToDoModel]? = nil
+        let task = session.dataTask(with: request) {data, response, error in
+            do {
+                if (data != nil){
+                    try fetchedModels = decoder.decode([ToDoModel].self, from: data!)
+                }
+            } catch {print(error)}
+            group1.leave()
+        }
+        
+        group1.enter()
+        queue1.sync {
+            task.resume()
+        }
+        
+        group1.notify(queue: .main) {
+            if (fetchedModels != nil) {
+                self.call1Value = fetchedModels![0].title
+            } else {self.call1Value = "Не удалось загрузить данные"}
+        }
+        return
+    }
+    
+    func call2() {
+        let group2 = DispatchGroup()
+        let queue2 = DispatchQueue(label: "call2", qos: .utility)
+        let decoder = JSONDecoder()
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 10
+        var request = URLRequest(url: url2)
+        request.httpMethod = "GET"
+        let session = URLSession(configuration: config)
+        var fetchedModels: PlanetModel? = nil
+        let task = session.dataTask(with: request) {data, response, error in
+            do {
+                if (data != nil){
+                    try fetchedModels = decoder.decode(PlanetModel.self, from: data!)
+                }
+            } catch {print(error)}
+            
+            group2.leave()
+        }
+        
+        group2.enter()
+        queue2.sync {
+            task.resume()
+        }
+        
+        group2.notify(queue: .main) {
+            if (fetchedModels != nil) {
+                self.call2Value = "Планета " + fetchedModels!.planet + " имеет период обращения " + fetchedModels!.orbitalPeriod
+            } else {self.call2Value = "Не удалось загрузить данные"}
+        }
+        return
     }
 }
